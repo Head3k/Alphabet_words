4567c2d47e3fd7ab455e4bdc2264b0857eaf4d1b
He3k_Kubuntu <Nickolablaniy@mail.ru>
2020-06-22 03:47:13 +0700

    New structure

--- src/dev.c
+++ src/dev.c
@@ -1,101 +1,76 @@
-
 #include <string.h>
 #include <stdio.h>
 
-int sort(char* k1,char* k2){
-    int x=0;
-    while(k1[x]==k2[x] && x<strlen(k1) && x<strlen(k2)){
-        x++;
-    }
-    //printf("\nk1==%c", k1[x]);
-   // printf("\nk2==%c", k2[x]);
-    
-    if (x==strlen(k1)) return 0;
-    if (x==strlen(k2)) return 1;
-    if (k1[x]>k2[x]) return 1;
-    return 0;
-
-
+int sort(char* k1,char* k2){  // function for decisions move for two words
+    int x=0; // init var for enumeration symbol in the words
+    while(tolower(k1[x])==tolower(k2[x]) && x<strlen(k2)){  // cicle for check symbols and choice next
+        x++; // var for next symbol int the word
+    } //
+    if (x==strlen(k1)) return 0;  // condition for move two words
+    if (x==strlen(k2)) return 1;  // condition for move two words
+    if (tolower(k1[x])>tolower(k2[x])) return 1;  // condition for move two words
+    return 0; // return NULL
 }
 
-char computing(char *array, int count_words){
-    int i=0,j=0,k=0;
-    char array_char[count_words][16]; // [words][simbol]
-    while (i<count_words){
-        memset(array_char[i],0,16);
-        i++; //clean memory
-    }
-    for(i=0;i<count_words;i++){
-        j=0;
-        while(i+j<count_words && array[i+j]!=' '){ // separated
-            array_char[k][j]=array[i+j];
-            j++;
-            printf("\narray_char= %s",array_char[k]); // print words
+char computing(char *array, int count_words, int count_symbols){  // function for symbols -> words and move words in array
+    int i=0,j=0,k=0;  // init var for cicle
+    char array_char[count_words][16];   // init general array  
+    while (i<count_words){             // cicle general array with data
+        memset(array_char[i],0,16);   // clear memory array
+        i++;                         // clean memory array
+    }                               //
+    for(i=0;i<count_symbols;i++){  // cicle for enumeration symbols
+        while(i+j<count_symbols && array[i+j]!=' ' && array[i+j]!=','){  // cicle for symbols -> words
+            array_char[k][j]=array[i+j]; // symbols -> words
+            j++; // var for next symbol in words
         }
-        k++;
-        i+=j;
-    } // get array_char with words
-
-    j=0; char tmp[16];
-    while(j<count_words){
-        i=0;
-        while (i<count_words){
-            printf("\nnod=%s",array_char[0]);
-            if(sort(array_char[i],array_char[i+1])){
-                strcpy(tmp,array_char[i]); printf("\ntmp-->%s",tmp);
-                strcpy(array_char[i],array_char[i+1]);
-                strcpy(array_char[i+1],tmp);
-                printf("\nYE%s",array_char[0]);
-            }
-        i++;
+        i+=j; // save number last symbol
+        k++; // var for next words in array
+        j=0; // clear number last symbol
+    }  // 
+    j=0;
+    char tmp[16];  // init var for buble sort array
+    while(j<(count_words)){  // cicle for retry 'cicle for enumeration words'
+        i=0;  // var for enumeration two words
+        while (i<(count_words) && (array_char[i][16] != '\0')){  // cicle for enumeration words 
+            if(sort(array_char[i],array_char[i+1])){  // condition sort two words
+                strcpy(tmp,array_char[i]);  // first words -> tmp  
+                strcpy(array_char[i],array_char[i+1]);  // second words -> first words
+                strcpy(array_char[i+1],tmp);  // tmp -> second words
+            } //
+        i++; // var for next two words
         }
-        // print array
+    j++;  // var for retry 'cicle for enumeration words'
     }
-}
+    i=0; // clean var for cicle for print general array
+    while (i<count_words){  // cicle for print general array
+        printf("\n%s", array_char[i]);  // print [i] word from the array
+        i++;  // var for next [i] word
+    }  //
+} //
 
-/*    array_char = array[i];
-    printf("%c", array_char);
-    array_char[i] = 'a';
-    strcat(array_char[i], *(array+i));
-    //array_char[i] += '\0';
-*/  //  printf("\n%c", array_char[0]);
+char string(int count_words){                     // function for enter data and check quantity symbols
+    int i=0;                                       // init var for clear clipboard
+    char *str;                                      // init string for data
+    int count_symbols=0;                             // init var for count symbols in the text
+    char array[count_words*16];                          // create char array
+    str = array;                                       // array --> str
+    printf("Please don't use a space after punctuation marks");  //
+    printf("\nEnter u text ==> ");                  //
+    scanf("\n%d", &i);                                  // scan for clean clipboard
+    while((*str ++= getchar ()) != '\n'){              // enter data
+        count_symbols++;                              // quantity symbols ++;
+        *str = '\0';                                 // add last mark string
+    }                                               //
+    computing(array, count_words, count_symbols);  // void function
+    return 0;                                     //
+}                                                //
 
-
-
-char string(int count_words){
-    int i;
-    char *str;
-    char array[count_words]; // Create char array 
-    str = array;
-    //printf("Please press Enter\n");
-    printf("Please enter u text ==>");
-    scanf("\n%d", &i);
-    while((*str ++= getchar ()) != '\n'){
-        *str = '\0'; // Add last mark string
-    }
-    //printf("%s\n", array);
-    computing(array, count_words);
-    return 0;
-}
-
-
-
-int main(){
-    int i=0,j=0;
-    int count_words = 0;
-    char array[count_words];
-    printf("Please enter maximum number of words in the text ==> ");
-    scanf("%d", &count_words);
-//  printf("count_words --> %d\n",count_words);
-    string(count_words); // void function
-
-/*  char array[count_words]; // Create char array 
-    char *str; // Init temporary mark
-    str = array;
-    printf("Please enter u text ==> \n");
-    while((*str ++= getchar ()) != '\n'){
-        *str = '\0'; // Add last mark string
-    }
-*/    
+int main(){                   // main
+    int count_words = 0;       // init var for count words in the text
+    printf("Enter maximum number of words in the init var for text ==> ");
+    scanf("%d", &count_words);  // enter count words in the text
+    count_words++;             // for E0F string
+    string(count_words);      // void function
     
 }
